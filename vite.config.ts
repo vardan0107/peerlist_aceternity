7dcd3c01e0f7f870130dfa77b6b785695885a098
MSDOMAIN1\dmuvval <vardan.muvvala@morningstar.com>
2025-05-09 00:53:24 +0530

    changed to vite . and react-router was using ssr

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,7 @@
-import { reactRouter } from "@react-router/dev/vite";
-import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
+import react from "@vitejs/plugin-react-swc";
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
-  base: "/peerlist_aceternity/",
+  plugins: [react()],
 });
